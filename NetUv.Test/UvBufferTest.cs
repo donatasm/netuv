7bc0f94160c6b3ebeb77b18c490c108983e509d3
Donatas Mažionis <donatas.mazionis@gmail.com>
2013-10-06 23:33:43 +0300

    Buffer tests

---
+++ NetUv.Test/UvBufferTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NetUv.Test
+{
+    [TestFixture]
+    internal sealed class UvBufferTest
+    {
+        [Test]
+        public void NullBufferThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new UvBuffer(null, 0, 1));
+            Assert.AreEqual("buffer", exception.ParamName);
+        }
+
+        [Test]
+        public void BufferLengthIs0ThrowsArgumentNullException()
+        {
+            var exception = Assert.Throws<ArgumentException>(
+                () => new UvBuffer(new byte[0], 0, 1));
+            Assert.AreEqual("buffer", exception.ParamName);
+        }
+
+        [TestCase(2, Description = "Offset is greater than length")]
+        [TestCase(1, Description = "Offset is equal to length")]
+        [TestCase(-1, Description = "Offset is less than 0")]
+        public void OffsetThrowsArgumentOutOfRangeException(int offset)
+        {
+            var buffer = new byte[1];
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                () => new UvBuffer(buffer, offset, buffer.Length));
+            Assert.AreEqual("offset", exception.ParamName);
+        }
+
+        [TestCase(2, Description = "Count is greater than buffer length")]
+        [TestCase(0, Description = "Count equals 0")]
+        [TestCase(-1, Description = "Count is less than 0")]
+        public void CountThrowsArgumentOutOfRangeException(int count)
+        {
+            var buffer = new byte[1];
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                () => new UvBuffer(buffer, 0, count));
+            Assert.AreEqual("count", exception.ParamName);
+        }
+
+        [TestCase(0, 4)]
+        [TestCase(1, 3)]
+        [TestCase(2, 2)]
+        public void OffsetCountThrowsArgumentOutOfRangeException(int offset, int count)
+        {
+            var buffer = new byte[3];
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new UvBuffer(buffer, offset, count));
+        }
+    }
+}
--- NetUv/UvBuffer.cpp
+++ NetUv/UvBuffer.cpp
@@ -4,6 +4,31 @@ namespace NetUv
 {
     UvBuffer::UvBuffer(array<byte>^ buffer, int offset, int count)
     {
+        if (buffer == nullptr)
+            throw gcnew ArgumentNullException("buffer");
+
+        if (buffer->Length == 0)
+            throw gcnew ArgumentException("Buffer length should be greater than 0.",
+                "buffer");
+
+        if (offset < 0)
+            throw gcnew ArgumentOutOfRangeException("offset",
+                "Offset should be greater than or equal to 0.");
+        if (offset >= buffer->Length)
+            throw gcnew ArgumentOutOfRangeException("offset",
+                "Offset should be less than buffer length.");
+
+        if (count < 1)
+            throw gcnew ArgumentOutOfRangeException("count",
+                "Count should be greater than 0.");
+        if (count > buffer->Length)
+            throw gcnew ArgumentOutOfRangeException("count",
+                "Count should be less than or equal to buffer length.");
+
+        if (offset + count > buffer->Length)
+            throw gcnew ArgumentOutOfRangeException(
+                "Offset + count should be less than or equal to buffer length.");
+
         _buffer = buffer;
         _offset = offset;
         _count = count;
